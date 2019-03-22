53ea28a329687b222fd428088a17c209107f5a69
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 16:42:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-
+  attendees = []
+  array.each do |name|
+    attendees << "Hello, my name is #{name}."
+  end
+  return attendees
 end
